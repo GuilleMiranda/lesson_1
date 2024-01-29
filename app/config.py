9b87d6b17bb9c31d69d3ea35f15cbc06ff507bfb
Guille <gdmn3000@gmail.com>
2024-01-28 21:39:54 -0300

    Oatuh2 and config files back to normal

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     db_username: str
     secret_key: str
     algorithm: str
-    access_token_expiry_minutes: str
+    access_token_expiry_minutes: int
 
     class Config:
         env_file = ".env"
