a17185d22216d422e60cc0aa93aebf842e453615
Guille <gdmn3000@gmail.com>
2024-01-29 21:30:15 -0300

    Try with start.sh again

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-python -m alembic upgrade head
+pip install -r /app/requirements.txt
+
+cd /app
+echo $(ls)
+python3 -m alembic upgrade head
 
 uvicorn app.main:app --host "0.0.0.0" --port 10000
