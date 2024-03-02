1d948661e393122d44f48caccce2b60423a7121c
Guille <gdmn3000@gmail.com>
2024-03-02 00:33:27 -0300

    MAke all env vars optional

--- app/config.py
+++ app/config.py
@@ -1,15 +1,15 @@
 from pydantic_settings import BaseSettings
-
+from typing import Optional
 
 class Settings(BaseSettings):
-    db_hostname: str
-    db_port: str
-    db_password: str
-    db_name: str
-    db_username: str
-    secret_key: str
-    algorithm: str
-    access_token_expiry_minutes: int
+    db_hostname: Optional[str]
+    db_port: Optional[str]
+    db_password: Optional[str]
+    db_name: Optional[str]
+    db_username: Optional[str]
+    secret_key: Optional[str]
+    algorithm: Optional[str]
+    access_token_expiry_minutes: Optional[int]
 
     # class Config:
     #     env_file = ".env"
